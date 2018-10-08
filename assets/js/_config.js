253d67a5d8ad0b77c9d237c0a13071d7295e7199
Nikola Babic <bbcnkl@gmail.com>
2018-10-08 11:44:56 +0200

    Update _config.js
    default image link

--- assets/js/_config.js
+++ assets/js/_config.js
@@ -1,8 +1,8 @@
 var
   /* Countdown */
-  site_launch_date = '2016/12/31 00:00', 	// Date of site launch
+  site_launch_date = '2019/12/31 00:00', 	// Date of site launch
 
   /* Image Background */
-  snowflakesBackgroundImage = ''; // px per tick
+  snowflakesBackgroundImage = 'https://raw.githubusercontent.com/bbcnkl/counter-website/master/assets/img/snow.png'; // px per tick
 
 
