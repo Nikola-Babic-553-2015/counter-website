513278a67d8373ba1781c6aec1c80570e7491c52
Nikola Babic <bbcnkl@gmail.com>
2018-10-09 01:44:08 +0200

    Update config.js
    for github pages

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,6 @@ var
   site_launch_date = '2019/12/31 00:00', 	// Date of site launch
 
   /* Image Background */
-  snowflakesBackgroundImage = 'https://raw.githubusercontent.com/bbcnkl/counter-website/master/assets/img/snow.png'; // px per tick
+  snowflakesBackgroundImage = '../img/snow.png'; // px per tick
 
 
